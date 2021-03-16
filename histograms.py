3d81f7878e0eb8124b99a73c6d7e6359797da5fd
Sefa Yapıcı <mustafasefayapici@gmail.com>
2021-03-16 23:42:30 +0300

    Update histograms.py

--- histograms.py
+++ histograms.py
@@ -26,28 +26,23 @@ for category in category_list:
     category_between_path = input_directory+"//"+category +"_"+"between_chain.csv"
     category_within_path = input_directory+"//"+category +"_"+"within_chain.csv"
     
-    if os.path.exists(category_between_path) and os.path.exists(category_between_path):
-        between = pd.read_csv(category_between_path)
+    if os.path.exists(category_between_path) and os.path.exists(category_between_path): # if both of them exists, we create a Histograms
+        between = pd.read_csv(category_between_path) 
         within = pd.read_csv(category_within_path)
         
         merged = pd.merge(between,within,how="inner",on = ["Code","Chain_name"])
-    
-        for chain in merged.Chain_name.unique(): 
-            chain_df = merged[merged["Chain_name"] == chain]
+        within_chain_data = within.Share_of_identical_within
+        between_chain_data = between.Share_of_identical_between
+        
             
-            within_chain_data = chain_df.Share_of_identical_within
-            between_chain_data = chain_df.Share_of_identical_between
-            
-            fig,ax = plt.subplots()
-            
-            plt.hist(within_chain_data, alpha=0.8, label='within_chain',color="blue")
-            plt.hist(between_chain_data, alpha=0.8, label='between_chain',color="red")
-            plt.legend(loc='upper right')
-            
-            plt.title('Share of Identical Prices for '+ category +" in " + chain )
-            plt.ylabel('Pair Count')
-            plt.xlabel('Share of Identical Prices')
-            plt.savefig(output_directory+"\\"+chain+"_"+category+".png", dpi=200) # change dpi to change picture size
-                        
-                
+        plt.hist(within_chain_data, alpha=0.8, label='within_chain',color="blue") # alpha value is the transperancy value of the bars, increase to make them opaque 
+        plt.hist(between_chain_data, alpha=0.8, label='between_chain',color="red")
+        
+        plt.legend(loc='upper right')
+        
+        plt.title('Share of Identical Prices for '+ category)
+        plt.ylabel('Pair Count')
+        plt.xlabel('Share of Identical Prices')
+        plt.savefig(output_directory+"\\"+category+".png", dpi=200) # change dpi to change picture size
+        plt.close()                
         
