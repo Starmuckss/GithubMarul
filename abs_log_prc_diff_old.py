36474ffcfc6e16d6993b2422fd466453b0c76ef6
S. Mehmet Ozsoy <smozsoy@hotmail.com>
2021-05-07 21:32:31 +0300

    renamed
    I have changed names of the files

--- abs_log_prc_diff_old.py
+++ abs_log_prc_diff_old.py
@@ -5,8 +5,9 @@ Find average log prices of point of sale data at product level
  and take the absolute difference with other PoS, for both between and within. 
  within: PoS from the same chain
  between: PoS from different chains
-Second metric in Uniform Pricing in US Retail Chains@author: HP
-rename the file: abs_log_prc_diff !!!!!!! problem on outer merge
+Second metric in Uniform Pricing in US Retail Chains 
+@author: Sefa
+rename the file: abs_log_prc_diff
 """
 import pandas as pd
 import os
@@ -16,16 +17,15 @@ import time
 from itertools import combinations
 import numpy as np
 from collections import defaultdict
-import random
 
 root_directory = "C:\\Users\\HP\\Desktop\\11" # root directory contains everything
 dates = os.listdir(root_directory) # Assumed date files are directly under root directory
 dir_path = os.path.dirname(os.path.realpath(__file__))
-output_directory = dir_path+"\\log_average_abs_difference" # Data will be printed out here
+output_directory = dir_path+"\\abs_log_prc_diff" # Data will be printed out here
 if not os.path.exists(output_directory): # create the folder if not exists already
     os.mkdir(output_directory)
 
-intermediate_output_directory = dir_path + "\\Log_average_for_abs_diff" # Preprocessed data will be recorded here
+intermediate_output_directory = dir_path + "\\temp_abs_log_prc_diff" # Preprocessed data will be recorded here
 if not os.path.exists(intermediate_output_directory): # create the folder if not exists already
     os.mkdir(intermediate_output_directory) 
 
@@ -104,7 +104,7 @@ def single_branch_prices_category(directory,date):
     
     return df[["Name","Code",date]]
 
-category_select = category_list  #for all categories use category_list
+category_select = ["sut"]
 # find pos from 3 different days, then merge them
 all_posF = find_distinct_pos(root_directory+"\\"+dates[0]) # first day 
 all_posM = find_distinct_pos(root_directory+"\\"+dates[ len(dates)//2 ]) #middle day
@@ -116,7 +116,7 @@ for category in all_posL.keys():
     all_pos_total[category] = all_posF[category] + all_posM[category] + all_posL[category] # combine all
     
     pos_dates_fixed = [ pos[:len(root_directory)+1] + dates[0] + pos[len(root_directory) + 3:]  for pos in all_pos_total[category]] #fix the date in first date so
-                                                                                                                                    #we can find duplicates (all dates are 01 now)
+                                                                                                                                    #we can find duplicates
     all_pos_total[category] = list(set(pos_dates_fixed)) #  drop duplicates by taking the set first then turning it into a regular list
 
 
@@ -124,63 +124,42 @@ for category in all_posL.keys():
 dict_of_pairs = get_pairs(all_pos_total) # pairs 
 
 count_pairs_within = defaultdict(list) # Allows us to count how many pairs for a particular chain
-count_pairs_between = defaultdict(list) # keys: "chain_name+"_"+category"("sariyer_sut"), values: list of pairs for that chain and category
+count_pairs_between = defaultdict(list)
 
-sampled_dict_of_pairs = defaultdict(list) # preprocessed data will be saved here keys:category values: list contains pairs
-
-# the for loop below:
-    # count all the pairs in chains with respect to category using count_pairs_between and count_pairs_within
-    # if count is greater than 200, take a random sample of size 200 and save to sample_dict_of_pairs
-    # else leave it as it is and save it to sample_dict_of_pairs
+sample_dict_of_pairs = defaultdict(list) # preprocessed data will be saved here keys:category values: list contains pairs
+ 
+# Below in the for loop: we create a panel data (all products in a category & prices observed in several days)
+# than we take average over days
+# calculate the difference of averages, across pairs
+# save the averages in pkl files.
 start_preprocessing = time.time()
 for category in category_select:
-    chain_name_within = set()
-    chain_name_between = set()
-    for pair in dict_of_pairs[category]:
+    for pair in dict_of_pairs[category]:                 
+        pair = [*pair]
         pos_name_1 = pair[0][pair[0].find(dates[0]) + len(dates[0])+1: pair[0].find(category)-1] # returns cagdas-chain-xxxx
         chain_name_1 = pos_name_1.split("-")[0] # returns cagdas
         
         pos_name_2 = pair[1][pair[1].find(dates[0]) + len(dates[0])+1: pair[1].find(category)-1] # returns cagdas-chain-xxxx
         chain_name_2 = pos_name_2.split("-")[0]
         
-        if chain_name_1 == chain_name_2: # if chain_names are same, pair is within
-            count_pairs_within[chain_name_1+"_"+category] += [pair] # "sariyer_sut" : [("C:\Users\HP\Desktop\11\01\seyhanlar-market-erenkoy-7502\peynirler.pkl",C:\Users\HP\Desktop\11\01\sariyer-market-acarkent-4823\peynirler.pkl)]
-            chain_name_within.add(chain_name_1) #
+        chain_names_of_pair = [chain_name_1,chain_name_2]
         
-        else: # if chain_names are different, pair is between 
-            count_pairs_between[chain_name_1+"_"+category] += [pair] # "oruc_sut" : ["C:\Users\HP\Desktop\11\01\oruc-market-icmeler-5012\peynirler.pkl",C:\Users\HP\Desktop\11\01\sariyer-market-acarkent-4823\peynirler.pkl]
-            count_pairs_between[chain_name_2+"_"+category] += [pair] # "sariyer_sut" : ["C:\Users\HP\Desktop\11\01\oruc-market-icmeler-5012\peynirler.pkl",C:\Users\HP\Desktop\11\01\sariyer-market-acarkent-4823\peynirler.pkl]
-            chain_name_between.add(chain_name_1)
-            chain_name_between.add(chain_name_2)
-    # For both of the within and between
-    # IF chain has more than 200 pairs in that category, sample it, then put them into sampled_dict_of_pairs for further use
-    # if chain has less than 200 pairs in that category, put the pairs without change into sampled_dict_of_pairs for further use 
-    for chain_name in chain_name_within: 
-        if len(count_pairs_within[chain_name+"_"+category])>=200:  
-            sample = random.sample(count_pairs_within[chain_name+"_"+category],200)
-            sampled_dict_of_pairs[category] += sample
+        # Check if we reached maximum pair count. If we are already at max, skip the preprocessing step
+        if chain_name_1 == chain_name_2:
+            if len(count_pairs_within[chain_names_of_pair[0]+"_"+category])>=200: 
+                continue
         else:
-            sampled_dict_of_pairs[category] += count_pairs_within[chain_name+"_"+category]
-    for chain_name in chain_name_between:
-        if len(count_pairs_between[chain_name+"_"+category])>=200: 
-            sample = random.sample(count_pairs_between[chain_name+"_"+category],200)
-            sampled_dict_of_pairs[category] += sample
-        else:
-            sampled_dict_of_pairs[category] += count_pairs_between[chain_name+"_"+category]
-            
-# Below in the for loop: we create a panel data (all products in a category & prices observed in several days)
-# than we take average over days
-# calculate the difference of averages, across pairs
-# save the averages in pkl files.
-    
-for category in category_select:
-    for pair in sampled_dict_of_pairs[category]:                 
-        for pos in pair: # pos is pair[0] or pair[1]
+            if len(count_pairs_between[chain_names_of_pair[0]+"_"+category])>=200 and len(count_pairs_between[chain_names_of_pair[1]+"_"+category])>=200: 
+                continue # I used "and" to be safe. I have the concern for ex. pair : cagdas-aypa
+                         # cagdas has 200 pairs but aypa has 100, in order not to limit aypa, I took this pair into consideration but with that, cagdas has more than 200
+                         # burada belki farklı bir yol izlenebilir, konuşabiliriz
+        
+        for pos in pair:
             # Get chain name, from .pkl path. 
 
             pos_name = pos[pos.find(dates[0]) + len(dates[0])+1: pos.find(category)-1] # returns cagdas-chain-xxxx
             chain_name = pos_name.split("-")[0]
-            log_average_df_directory = intermediate_output_directory + "\\" + pos_name + "_" + category+ ".pkl"
+            log_average_df_directory = intermediate_output_directory + "\\" + pos_name+"_"+category+ ".pkl"
 
             if not os.path.exists(log_average_df_directory):
                 pos_daily = [] # pos_daily will be populated by daily dataframes for that point of sale
@@ -188,27 +167,20 @@ for category in category_select:
     
                 try:
                     for date in dates:
-                        daily_dataframe = single_branch_prices_category(pos,date)
-                        if len(daily_dataframe) > 0: # AttributeError: Can only use .str accessor with string values! solves this!!!
-                            pos_daily.append(daily_dataframe)   # time-series price data from pos_1 pos of the list
-                            dates_in_analysis.append(date)
-                        
+                        pos_daily.append(single_branch_prices_category(pos,date))   # time-series price data from pos_1 pos of the list
+                        dates_in_analysis.append(date)
+    
                 except FileNotFoundError:
                     pass 
             
             
-                if len(dates_in_analysis) > 0 :  
-                    pos_merge = reduce(lambda left,right: pd.merge(left,right,on='Code',how="outer",suffixes=("","_y")),pos_daily) # OUTER merge SIKINTILI
-                    cols = ["Name","Name_y"]
-                    pos_merge["Names"] = pos_merge[cols].apply(lambda x: ','.join(x.dropna()), axis=1)  # Join all the names in one column named "Names"
-                    pos_merge["Names"] = pos_merge["Names"].str.split(",").str.get(0) # there will be multiple names, take the first.
-                    pos_merge.drop(cols,axis="columns",inplace=(True))
-                    pos_merge.rename(columns={"Names":"Name"},inplace = True)
+                if len(dates_in_analysis) > 0:  
+                    pos_merge = reduce(lambda left,right: pd.merge(left,right,on='Code',how="outer",suffixes=("","_y")),pos_daily) # OUTER merge
                 else:
                     continue
             
             #drop duplicate columns
-                pos_merge = pos_merge.loc[:,~pos_merge.columns.duplicated()] 
+                pos_merge = pos_merge.loc[:,~pos_merge.columns.duplicated()]
                 
                 pos_merge["Num_days_sold"] = [len(v[pd.notna(v)].tolist()) for v in pos_merge[dates_in_analysis].values] # day count of a particular product sold
                 pos_merge["Mean_log_price"] = [statistics.mean(v[pd.notna(v)].tolist()) for v in pos_merge[dates_in_analysis].values] # Mean_price as a column
@@ -217,7 +189,14 @@ for category in category_select:
                 # Output the preprocessed data
                 pos_merge[["Name","Code" ,"Mean_log_price"]].to_pickle(log_average_df_directory) 
         
-        
+        # count the pairs
+        if chain_names_of_pair[0] == chain_names_of_pair[1]:
+            count_pairs_within[chain_names_of_pair[0]+"_"+category] += [pair]
+        else:
+            count_pairs_between[chain_names_of_pair[0]+"_"+category] += [pair]
+            count_pairs_between[chain_names_of_pair[1]+"_"+category] += [pair]
+        sample_dict_of_pairs[category] += [pair]
+       
 end_preprocessing = time.time()
 
 print("preprocessing took " + str(end_preprocessing-start_preprocessing)+" seconds")   
@@ -230,7 +209,7 @@ for category in category_select:
     within = defaultdict(list)  # there are empty dictionaries
     between = defaultdict(list)
 
-    for pair in sampled_dict_of_pairs[category]:                 
+    for pair in sample_dict_of_pairs[category]:                 
         pos_name1 = pair[0][pair[0].find(dates[0]) + len(dates[0])+1: pair[0].find(category)-1] # returns cagdas-chain-xxxx
         chain_name1 = pos_name1.split("-")[0] # returns cagdas
         
@@ -238,11 +217,9 @@ for category in category_select:
         chain_name2 = pos_name2.split("-")[0]
         
         # since we already calculated averages, we read the data directly and start merging both pos in the pair
-        try:    
-            pos_1 = pd.read_pickle(intermediate_output_directory + "\\" + pos_name1+"_"+category+ ".pkl")   
-            pos_2 = pd.read_pickle(intermediate_output_directory + "\\" + pos_name2+"_"+category+ ".pkl")
-        except:
-            continue
+        pos_1= pd.read_pickle(intermediate_output_directory + "\\" + pos_name1+"_"+category+ ".pkl")   
+        pos_2 = pd.read_pickle(intermediate_output_directory + "\\" + pos_name2+"_"+category+ ".pkl")
+        
         final_merge_for_pair = pd.merge(pos_1,right=pos_2,on = "Code") # inner merge on code 
         final_merge_for_pair["absolute_difference"] = np.abs(final_merge_for_pair["Mean_log_price_x"] - final_merge_for_pair["Mean_log_price_y"]) # abs difference of mean prices
         final_merge_for_pair.rename(columns = {"Name_x" : "Name" },inplace = True)
@@ -305,35 +282,28 @@ for category in category_select:
     between_df = pd.DataFrame()
     within_df = pd.DataFrame()
     
-    # NOW IN LONG FORMAT: df.columns = name,code,pair_count,between_abs_difference,chain_name  
     for chain in between.keys():
         if len(between_df) == 0:
             between_df=  between[chain]
-            between_df["between_abs_difference"] = between_df["absolute_difference"] / between_df["pair_count"] # abs_difference is divided to pair count, reported under chain name
-            between_df.drop(["absolute_difference"],axis =1, inplace = True)
-            between_df["chain_name"] = chain
+            between_df[chain] = between_df["absolute_difference"] / between_df["pair_count"] # abs_difference is divided to pair count, reported under chain name
+            between_df.drop(["absolute_difference","pair_count"],axis =1, inplace = True)
         else:
             df = between[chain] 
-            df["between_abs_difference"] = df["absolute_difference"] / df["pair_count"]
-            #between_df = pd.merge(between_df,df[["Code","Name","between_abs_difference"]],on = ["Name","Code"],how = "outer")
-            df.drop(["absolute_difference"],axis =1, inplace = True)
-            df["chain_name"] = chain
-            between_df = between_df.append(df)
+            df[chain] = df["absolute_difference"] / df["pair_count"]
+            between_df = pd.merge(between_df,df[["Code","Name",chain]],on = ["Name","Code"],how = "outer")
+    
     for chain in within.keys():
         if len(within_df) == 0:
             within_df=  within[chain]
-            within_df["within_abs_difference"] = within_df["absolute_difference"] / within_df["pair_count"] # abs_difference is divided to pair count, reported under chain name
-            within_df.drop(["absolute_difference"],axis =1, inplace = True)
-            within_df["chain_name"] = chain
+            within_df[chain] = within_df["absolute_difference"] / within_df["pair_count"]
+            within_df.drop(["absolute_difference","pair_count"],axis =1, inplace = True)
+
         else:
             df = within[chain] 
-            df["within_abs_difference"] = df["absolute_difference"] / df["pair_count"]
-            #within_df = pd.merge(within_df,df[["Code","Name","within_abs_difference"]],on = ["Name","Code"],how = "outer")
-            df.drop(["absolute_difference"],axis =1, inplace = True)
-            df["chain_name"] = chain
-            within_df = within_df.append(df)       
+            df[chain] = df["absolute_difference"] / df["pair_count"] 
+            within_df = pd.merge(within_df,df[["Code","Name",chain]],on = ["Name","Code"],how = "outer")        
             
-    between_df.to_csv(output_directory + "\\" + category + "_between.csv" )  # report as csv ex: deodorant-parfum_between.csv
+    between_df.to_csv(output_directory + "\\" + category + "_between.csv" )  # report as csv ex: deodorant-parfum_within.csv
     within_df.to_csv(output_directory + "\\" + category + "_within.csv" )  # report as csv ex:deodorant-parfum_within.csv
     end_category = time.time()
     print( category + " evaluated in: " + str(end_category-start_category) + " seconds")
